4c29402982775a7ec3b757143832eebf0a7a84f2
RadiumServices <107222533+RadiumServices@users.noreply.github.com>
2022-08-04 02:15:10 -0400

    Update sun-lock (best lock).lua

--- sun-lock (best lock).lua
+++ sun-lock (best lock).lua
@@ -8,7 +8,7 @@
 ╚═════╝░░╚═════╝░╚═╝░░╚══╝                                            
 ]]
 local NotifyLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/vKhonshu/intro/main/ui"))()
-NotifyLib.prompt('SUN LOCK - Q To Lock \ X to macro', 'Loading..', 5)
+NotifyLib.prompt('SUN LOCK - Q To Lock and X to macro', 'Loading..', 5)
 NotifyLib.prompt('SUN LOCK', ' Made by $un#3554', 5)
 
 
