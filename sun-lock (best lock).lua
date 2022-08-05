5e5b400171bdf045bd6502d191ccf0811a4985b1
RadiumServices <107222533+RadiumServices@users.noreply.github.com>
2022-08-04 23:07:13 -0400

    Update sun-lock (best lock).lua

--- sun-lock (best lock).lua
+++ sun-lock (best lock).lua
@@ -7,7 +7,16 @@
 ██████╔╝╚██████╔╝██║░╚███║
 ╚═════╝░░╚═════╝░╚═╝░░╚══╝                                            
 ]]
-
+while true do
+local Animate = game.Players.LocalPlayer.Character.Animate
+Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=782841498"
+Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=782841498"
+Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=616168032"
+Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616163682"
+Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=1083439238"
+game.Players.LocalPlayer.Character.Humanoid.Jump = false
+wait(1)
+end
 --[[
 animations
 ]]
